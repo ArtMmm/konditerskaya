19011e9f20767e815955aead5c0f2362b7e5a76e
ArtMmm <60774082+ArtMmm@users.noreply.github.com>
2020-02-19 11:44:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("#carousel1").owlCarousel({
+    autoplay: true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    animateOut: 'bounceOut',
+    animateIn: 'flipInX',
+    items:1,
+    loop: true,
+    nav: true,
+    dots: false
+  });
+});
